723bf880191a7f9ef18bdae67308de7433336d10
DJwhoCODES <jaindevanshu824@gmail.com>
2026-04-18 14:51:19 +0530

    server creation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/djwhocodes/d-cache/internal/protocol"
+	"github.com/djwhocodes/d-cache/internal/transport"
+)
+
+func main() {
+	server := transport.NewTCPServer(":8080")
+
+	err := server.Start(connectionHandler)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func connectionHandler(conn net.Conn) {
+	transport.HandleConnection(conn, handleRequest)
+}
+
+func handleRequest(req *protocol.Request) *protocol.Response {
+	return &protocol.Response{
+		Header: protocol.Header{
+			Command:   req.Header.Command,
+			RequestID: req.Header.RequestID,
+		},
+		Status: protocol.StatusOK,
+		Value:  []byte("OK"),
+	}
+}
