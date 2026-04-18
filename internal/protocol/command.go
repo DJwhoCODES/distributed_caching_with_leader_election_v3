0180961dd7b525b06c2f65bce2c884643095133b
DJwhoCODES <jaindevanshu824@gmail.com>
2026-04-18 11:08:41 +0530

    defined request and response structures

--- internal/protocol/command.go
+++ internal/protocol/command.go
@@ -1,1 +1,15 @@
 package protocol
+
+type Request struct {
+	Header Header
+	Key    []byte
+	Value  []byte
+	TTL    uint32
+}
+
+type Response struct {
+	Header Header
+	Status Status
+	Value  []byte
+	Error  string
+}
