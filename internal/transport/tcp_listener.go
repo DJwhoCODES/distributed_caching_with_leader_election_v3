a87906f96483aa5e8bdc84598b16f3355712922b
DJwhoCODES <jaindevanshu824@gmail.com>
2026-04-18 11:23:13 +0530

    Start server and listen to connections

--- internal/transport/tcp_listener.go
+++ internal/transport/tcp_listener.go
@@ -1,1 +1,36 @@
 package transport
+
+import (
+	"log"
+	"net"
+)
+
+type TCPServer struct {
+	Addr string
+}
+
+func NewTCPServer(addr string) *TCPServer {
+	return &TCPServer{
+		Addr: addr,
+	}
+}
+
+func (s *TCPServer) Start(handler func(net.Conn)) error {
+	listener, err := net.Listen("tcp", s.Addr)
+	if err != nil {
+		return err
+	}
+
+	log.Println("TCP server listening on", s.Addr)
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Println("accept error:", err)
+			continue
+		}
+
+		go handler(conn)
+	}
+}
