f9a8362334418a128a0f2f3ca3c049d170da6455
DJwhoCODES <jaindevanshu824@gmail.com>
2026-04-18 11:07:54 +0530

    declared command constants

--- internal/protocol/constants.go
+++ internal/protocol/constants.go
@@ -1,1 +1,28 @@
 package protocol
+
+const Version uint8 = 1
+
+type Command uint8
+
+const (
+	CmdGet Command = iota + 1
+	CmdSet
+	CmdDelete
+	CmdMGet
+	CmdMSet
+	CmdPing
+
+	CmdSync
+	CmdVoteRequest
+	CmdVoteResponse
+	CmdHeartbeat
+)
+
+type Status uint8
+
+const (
+	StatusOK Status = iota
+	StatusNotFound
+	StatusError
+	StatusRedirect
+)
