a8d2a8e60c6862da698f206b70796ac5ff330c39
DJwhoCODES <jaindevanshu824@gmail.com>
2026-04-18 11:09:18 +0530

    defined header structure

--- internal/protocol/header.go
+++ internal/protocol/header.go
@@ -1,1 +1,11 @@
 package protocol
+
+type Header struct {
+	Version       uint8
+	Command       Command
+	Flags         uint8
+	RequestID     uint32
+	PayloadLength uint32
+}
+
+const HeaderSize = 11
