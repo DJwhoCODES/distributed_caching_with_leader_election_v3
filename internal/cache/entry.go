63f0b87c42b6964904ec64c45d9052f5ee1a48b0
DJwhoCODES <jaindevanshu824@gmail.com>
2026-04-23 05:28:56 +0530

    built store and its access methods

--- internal/cache/entry.go
+++ internal/cache/entry.go
@@ -1,1 +1,16 @@
 package cache
+
+import "time"
+
+type Entry struct {
+	Value     []byte
+	ExpiresAt int64 //unix timestamp
+}
+
+func (e *Entry) IsExpired() bool {
+	if e.ExpiresAt == 0 {
+		return false
+	}
+
+	return time.Now().Unix() > e.ExpiresAt
+}
--- internal/cache/store.go
+++ internal/cache/store.go
@@ -1,1 +1,77 @@
 package cache
+
+import (
+	"sync"
+	"time"
+)
+
+type Store struct {
+	mu    sync.RWMutex
+	items map[string]*Entry
+}
+
+func NewStore() *Store {
+	return &Store{
+		items: make(map[string]*Entry),
+	}
+}
+
+func (s *Store) Set(key string, value []byte, ttl int32) {
+	var exp int64 = 0
+
+	if ttl > 0 {
+		exp = time.Now().Add(time.Duration(ttl) * time.Second).Unix()
+	}
+
+	s.mu.Lock()
+	s.items[key] = &Entry{
+		Value:     value,
+		ExpiresAt: exp,
+	}
+
+	s.mu.Unlock()
+}
+
+func (s *Store) Get(key string) ([]byte, bool) {
+	s.mu.Lock()
+	entry, ok := s.items[key]
+	s.mu.RUnlock()
+
+	if !ok {
+		return nil, false
+	}
+
+	if entry.IsExpired() {
+		s.Delete(key)
+		return nil, false
+	}
+
+	return entry.Value, true
+}
+
+func (s *Store) Delete(key string) {
+	s.mu.Lock()
+	delete(s.items, key)
+	s.mu.Unlock()
+}
+
+func (s *Store) StartJanitor(interval time.Duration) {
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			now := time.Now().Unix()
+
+			s.mu.Lock()
+
+			for k, v := range s.items {
+				if v.ExpiresAt != 0 && v.ExpiresAt < now {
+					delete(s.items, k)
+				}
+			}
+
+			s.mu.Unlock()
+		}
+	}()
+}
